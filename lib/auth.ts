6951ced12015fbb4c63f14b406809b105944c016
Ch Mahesh <chmahesh.dev@gmail.com>
2025-12-23 17:13:25 +0530

    custom signin page

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,21 +4,14 @@ import GoogleProvider from "next-auth/providers/google";
 
 export const NEXT_AUTH_CONFIG = {
     providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID ?? "",
-            clientSecret: process.env.GITHUB_SECRET ?? ""
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
-        }),
-      CredentialsProvider({
+        CredentialsProvider({
           name: 'Credentials',
           credentials: {
             username: { label: 'email', type: 'text', placeholder: '' },
             password: { label: 'password', type: 'password', placeholder: '' },
           },
           async authorize(credentials: any) {
+            console.log("credentials", credentials);
   
               return {
                   id: "user1",
@@ -27,6 +20,14 @@ export const NEXT_AUTH_CONFIG = {
                   email: "ramdomEmail"
               };
           },
+        }),
+         GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID ?? "",
+            clientSecret: process.env.GITHUB_SECRET ?? ""
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
@@ -44,7 +45,7 @@ export const NEXT_AUTH_CONFIG = {
           return session
       }
     },
-    pages: {
-        signIn: '/signin',
-    }
+    // pages: {
+    //     signIn: '/signin',
+    // },
   }
